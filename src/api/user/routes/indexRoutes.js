652bf040a1416f577d3f499686013f7101c1f51c
lalitonpoint <lalit@appsquadz.co>
2025-04-09 15:13:52 +0530

    .

--- src/api/user/routes/indexRoutes.js
+++ src/api/user/routes/indexRoutes.js
@@ -7,7 +7,7 @@ const servicesRoutes = require('./serviceRoutes');
 const { verifyToken } = require('../middleware/authentication');
 router.use('/otp', sendOtpRoutes);
 
-router.use('/user', verifyToken, userRoutes);
-router.use('/service', verifyToken, servicesRoutes);
+router.use('/user', userRoutes);
+router.use('/service', servicesRoutes);
 
 module.exports = router;
