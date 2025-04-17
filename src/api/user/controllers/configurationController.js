b09d75f4e40158dba47d25f711d9e2459d9a7238
lalitonpoint <lalit@appsquadz.co>
2025-04-17 15:37:19 +0530

    .

--- src/api/user/controllers/configurationController.js
+++ src/api/user/controllers/configurationController.js
@@ -3,10 +3,11 @@ const FAQ = require('../../../admin/models/faqManagement/faqModel');
 
 const getSettingData = async (req, res) => {
     try {
-        const [aboutUs, termsAndConditions, helpAndSupport, faq] = await Promise.all([
+        const [aboutUs, termsAndConditions, helpAndSupport, privacyAndPolicy, faq] = await Promise.all([
             settingModel.find({ aboutUs: { $exists: true } }).sort({ createdAt: -1 }),
             settingModel.find({ termsAndConditions: { $exists: true } }).sort({ createdAt: -1 }),
             settingModel.find({ helpSupport: { $exists: true } }).sort({ createdAt: -1 }),
+            settingModel.find({ privacyAndPolicy: { $exists: true } }).sort({ createdAt: -1 }),
             FAQ.find({ status: 1 }).sort({ createdAt: -1 })
         ]);
         // console.log(helpAndSupport[0].helpSupport);
@@ -17,6 +18,7 @@ const getSettingData = async (req, res) => {
                 aboutUs: aboutUs[0].aboutUs || null,
                 termsAndConditions: termsAndConditions[0].termsAndConditions || null,
                 helpAndSupport: helpAndSupport[0].helpSupport || null,
+                privacyAndPolicy: privacyAndPolicy[0].privacyAndPolicy || null,
                 faq
             }
         });
