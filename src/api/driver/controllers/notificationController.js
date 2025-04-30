5ca15afef710c910882ca9b54e52a6c6c0b14e3e
lalitonpoint <lalit@appsquadz.co>
2025-04-30 15:25:24 +0530

    api_token

--- src/api/driver/controllers/notificationController.js
+++ src/api/driver/controllers/notificationController.js
@@ -30,8 +30,7 @@ const assignOrderToDriver = async (req, res) => {
     const { orderId, driverId } = req.body;
 
 
-    // const driverToken = 'dRC3aC4CRMiP6H_08OOjQw:APA91bGVz_Ds2rWn4YWZFH8ssoek7iOUvACczieDBwVt0Kkc4wlkbxwGYT9944APWwM7q90tCn-bDHSWroQCSx6E6uTU62d9-7W8Tyh-wbQYbQpDH3fJnYQ';
-    const driverToken = 'czptey0uTCiGGmmAh7lsWU:APA91bFaKSTNfmJ5FXaXxJnQ0VmhTNzvk-i4KfOHJq-R17txaaH8c3K7R5PtlQ6Ek58HwWAr5JVIlfVx-Hn7Lbb_UnpLysO7kRT4ZZqxkZWFUJ53qYkmbtU';
+    const driverToken = 'dTL0_NH7SEGUfnuBemSwws:APA91bGNrUzxKwRUqgFcMqol9lMbWaglogn5alWSHI0c_weYRR2UN0ti875aIDTuXNMAN0c7HPYSBzxEg3Kwzvzv0RzhuDMFrOFyjWzQ8E70ARBylxEcWZE';
 
     // const driverToken = await getDriverToken(driverId); // Your DB logic
 
