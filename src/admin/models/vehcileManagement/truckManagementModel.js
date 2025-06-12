d3de650fce031ad1e1dbe3c2477ead01cbf33a14
lalitonpoint <lalit@appsquadz.co>
2025-06-12 17:08:23 +0530

    changevehicle image

--- src/admin/models/vehcileManagement/truckManagementModel.js
+++ src/admin/models/vehcileManagement/truckManagementModel.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose;
 const vehicleSchema = new Schema({
     name: { type: String, required: true, trim: true }, // Truck Name
     status: { type: Number, enum: [1, 2, 3], default: 1 }, // 1 = Active, 2 = Inactive , 3 => Delete
-    vechileImage: { type: String, trim: true }, // Vehicle Image URL/Path
+    vehicleImage: { type: String, trim: true }, // Vehicle Image URL/Path
     serviceType: { type: mongoose.Schema.Types.ObjectId, ref: 'services' },
     bodyType: { type: String, trim: true },
     tireType: { type: String, trim: true },
