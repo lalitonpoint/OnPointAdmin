ce3cb97c19dbc9edb3e6287fce60e2b91e9f3fd7
Raj Gupta <rajgupta10881088@gmail.com>
2025-06-23 12:15:04 +0530

    .

--- src/api/user/models/ftlPaymentModal.js
+++ src/api/user/models/ftlPaymentModal.js
@@ -19,18 +19,18 @@ const InitiatePaymentSchema = new mongoose.Schema({
     totalPayment: { type: Number, required: true },
     prePayment: { type: Number, bydefault: 0 },
     postPayment: { type: Number, bydefault: 0 },
-    estimatePrice: { type: Number, bydefault: 0 },
+    estimatePrice: { type: Number, bydefault: 0.00 },
     paymentMethod: { type: String },
     paymentGateway: { type: String },
     distance: { type: Number },
     duration: { type: String },
-    isBidding: { type: Number, enum: [0, 1], default: 0 }, // 0 => No Bid , 1 => Bidding
+    isBidding: { type: Number, enum: [0, 1], bydefault: 0.00 }, // 0 => No Bid , 1 => Bidding
     step: { type: Number, enum: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], default: 0 },
     isPartialPayment: { type: Number, enum: [0, 1, 2], default: 0 }, // 0 => FTL Without Bidding Payment , 1 => FTL Bidding Partial Payment , 2 => FTL Bidding Full Payment
     isAccepted: { type: Number, enum: [0, 1, 2, 3], default: 0 }, // 0 => Not Accepted , 1 => Accepted , 2 => Rejected , 3 => Bid
     orderStatus: { type: Number, enum: [0, 1, 2, 3, 4, 5], default: 0 }, // 0 => InProgress , 1 => Pickup, 2 => In Transit , 3 => Out for Delivery , 4 => Delivered , 5 => Cancelled
     transactionStatus: { type: Number, enum: [0, 1, 2, 3, 4, 5], default: 0 }, // 0 => Initiate, 1 => Complete , 2 => Pending , 3 => Failed , 4 => Refunded , 5 => Partial Payment
-    isWalletPay: { type: Number, enum: [0, 1], default: 0 }, // 0 => Online Payment, 1 => Wallet Pay
+    isWalletPay: { type: Number, enum: [0, 1], bydefault: 0.00 }, // 0 => Online Payment, 1 => Wallet Pay
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     preTransactionId: { type: String, default: "0" },
     postTransactionId: { type: String, default: "0" },
--- src/api/user/utils/fixedValue.js
+++ src/api/user/utils/fixedValue.js
@@ -1,3 +1,6 @@
-const toFixed = (val) => parseFloat(parseFloat(val || 0).toFixed(2));
+const toFixed = (val) => {
+    const num = parseFloat(val);
+    return isNaN(num) ? "0.00" : num.toFixed(2); // returns string like "4567.00"
+};
 
 module.exports = { toFixed };
