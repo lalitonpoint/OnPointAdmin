973c57b68eb876e399b37da9ffe9d0a0d6b8b284
Raj Gupta <rajgupta10881088@gmail.com>
2025-07-01 11:39:31 +0530

    .

--- src/admin/models/vehcileManagement/serviceManagementModel.js
+++ src/admin/models/vehcileManagement/serviceManagementModel.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const serviceSchema = new mongoose.Schema({
     serviceName: { type: String, required: true },
-    status: { type: Number, enum: [1, 2, 3], default: 1 }, // 1 = Active, 2 = Inactive , 3 => Delete
-    serviceImage: { type: String, required: true }, // Stores the path or URL of the image
+    status: { type: Number, enum: [1, 2, 3], default: 1 }, // 1 = Active, 2 = Inactive, 3 = Deleted
+    serviceImage: { type: String, required: true }, // Stores the image path or URL
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
-    serviceType: { type: Number, enum: [0, 1, 2, 3, 4], bydefault: 0 } // 1 => PTL , 2 => FTL Intercity , 3 => FTL Outer Station , 4 => Two Wheeler
+    serviceType: { type: Number, enum: [0, 1, 2, 3, 4], default: 0 } // 0 = default, 1 = PTL, 2 = FTL Intercity, etc.
 });
 
 module.exports = mongoose.model('services', serviceSchema);
