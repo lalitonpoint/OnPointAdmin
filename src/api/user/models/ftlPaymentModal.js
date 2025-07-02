ece46aa695b1c9b647327958726b8af2b5c0c7ce
Raj Gupta <rajgupta10881088@gmail.com>
2025-07-02 10:46:16 +0530

    .

--- src/api/user/models/ftlPaymentModal.js
+++ src/api/user/models/ftlPaymentModal.js
@@ -66,7 +66,7 @@ const InitiatePaymentSchema = new mongoose.Schema({
     driverId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'DriverProfile',
-        default: "null"
+        default: null
     }
 }, { timestamps: true });
 
